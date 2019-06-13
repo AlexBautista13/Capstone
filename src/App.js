e7081aa1af55ffba607ee780e4cdd0542b487928
alexb24 <lakers76767@gmail.com>
2019-06-13 14:55:32 -0700

    setup project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+      <div className="App">
+        <Navbar/>
+      </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/layout/SignedInLinks.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+
+const SignedInLinks = () => {
+    return (
+      <ul className="right">
+          <li><NavLink to='/'>New Project</NavLink></li>
+          <li><NavLink to='/'>Sign Out</NavLink></li>
+          <li><NavLink to='/' className='btn btn-floating pink lighten-1'>NN</NavLink></li>
+      </ul>
+
+    )
+}
+
+export default SignedInLinks
