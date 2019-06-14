16f450de07b4ba99c0739ce807dc1c1d57cae289
alexb24 <lakers76767@gmail.com>
2019-06-13 23:54:41 -0700

    finished the summary component and shortened down

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,33 +1,13 @@
 import React from 'react'
+import ProjectSummary from './ProjectSummary'
 
 const ProjectList = () => {
     return(
         <div className="project-list section">
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by the Net Ninja</p>
-                    <p className="grey-text">Jun 13</p>
-                </div> 
-            </div>
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by the Net Ninja</p>
-                    <p className="grey-text">Jun 13</p>
-                </div> 
-            </div>
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by the Net Ninja</p>
-                    <p className="grey-text">Jun 13</p>
-                </div> 
-            </div>
-
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
         </div>
     )
 }
---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ProjectSummary = () => {
+    return (
+        <div className="card z-depth-0 project-summary">
+            <div className="card-content grey-text text-darken-3">
+                <span className="card-title">Project Title</span>
+                <p>Posted by the Net Ninja</p>
+                <p className="grey-text">Jun 13</p>
+            </div> 
+        </div>
+        
+    )
+}
+
+export default ProjectSummary
