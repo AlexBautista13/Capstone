4debde93aed533fc76ea2c6e388639eaa83a681a
alexb24 <lakers76767@gmail.com>
2019-06-17 00:15:00 -0700

    install dep: for firebase data base and imported the Api key

---
+++ src/config/fbConfig.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+var config = {
+    apiKey: "AIzaSyBjOwZ-IW6I0WVzZrcg__qhZPjy11FvK4w",
+    authDomain: "ab-joker-app.firebaseapp.com",
+    databaseURL: "https://ab-joker-app.firebaseio.com",
+    projectId: "ab-joker-app",
+    storageBucket: "ab-joker-app.appspot.com",
+    messagingSenderId: "724561551453",
+    appId: "1:724561551453:web:ab680cd00fe39015"
+};
+// Initialize Firebase
+
+firebase.initializeApp(config);
+firebase.firestore().settings({timestampsInSnapshots: true });
+
+export default firebase;
+
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,13 +1,17 @@
 const initState = {
-    projects: [
-        {id: '1', title: 'help me find peach', content: 'blah blah blah'},
-        {id: '2', title: 'collect all the stars', content: 'blah blah blah'},
-        {id: '3', title: 'egg hunt with yoshi', content: 'blah blah blah'},
-    ]
+  projects: [
+    {id: '1', title: 'help me find peach', content: 'blah blah blah'},
+    {id: '2', title: 'collect all the stars', content: 'blah blah blah'},
+    {id: '3', title: 'egg hunt with yoshi', content: 'blah blah blah'}
+  ]
 }
 
-const ProjectReducer = (state =initState, action) => {
-    return state
-}
+const projectReducer = (state = initState, action) => {
+  switch (action.type) {
+    case 'CREATE_PROJECT':
+      console.log('create project', action.project);
+  }
+  return state;
+};
 
-export default ProjectReducer
+export default projectReducer;
