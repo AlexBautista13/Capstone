89942b600b9c580763cba748c651f786748c9941
alexb24 <lakers76767@gmail.com>
2019-06-18 22:14:02 -0700

    added user out and user in with firebase

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,7 +1,26 @@
-const initState = {}
-
-const authReducer = (state =initState, action) => {
-    return state
-}
-
-export default authReducer
+const initState = {
+    authError: null
+  }
+  
+  const authReducer = (state = initState, action) => {
+    switch(action.type){
+      case 'LOGIN_ERROR':
+        console.log('login error');
+        return {
+          ...state,
+          authError: 'Login failure'
+        }
+      case 'LOGIN_SUCCESS':
+        console.log('login successful');
+        return {
+          authError: null
+        }
+        case 'SIGNOUT_SUCCESS':
+            console.log('signout success')
+            return state;
+      default:
+        return state
+    }
+  };
+  
+  export default authReducer;
