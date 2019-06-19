e17f48dae9387ac533c46f0eeeee5be4c3bb29fd
alexb24 <lakers76767@gmail.com>
2019-06-18 21:17:08 -0700

    added firebaseReducer

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -7,8 +7,8 @@ const ProjectList = ({projects}) => {
     <div className="project-list section">
       { projects && projects.map(project => {
         return (
-          <Link to={'/project/' + project.id}>
-          <ProjectSummary project={project} key={project.id} />
+          <Link to={'/project/' + project.id} key={project.id}>
+          <ProjectSummary project={project}  />
           </Link>
         )
       })}  
--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,15 +1,15 @@
-    
 import authReducer from './authReducer'
 import projectReducer from './projectReducer'
 import { combineReducers } from 'redux'
 import { firestoreReducer } from 'redux-firestore';
+import { firebaseReducer } from 'react-redux-firebase'
 
 const rootReducer = combineReducers({
   auth: authReducer,
   project: projectReducer,
-  firestore: firestoreReducer
+  firestore: firestoreReducer,
+  firebase: firebaseReducer
 });
 
 export default rootReducer
 
-// the key name will be the data property on the state object
